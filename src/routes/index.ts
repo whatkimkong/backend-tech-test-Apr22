5c14601c1e7564290dc8a3f2942efd48b41f88fe
whatkimkong <whatkimkong@gmail.com>
2022-04-14 18:38:38 +0200

    feat: crud operations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
-import Log from '../middlewares/Log';
+import instanceRoutes from './instance.routes';
+import Log from '../error-handling/Log';
 
 const router = require("express").Router();
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json("/ get working fine");
+    res.status(200).json("Initial get route is working fine");
+    Log.info(res);
 });
 
-// router.get("/join", (req, res, next) => {
-//   res.json("All good in here");
-// });
+// ROUTES: 
+router.post('/:group/:id', instanceRoutes.createInstance);
+router.get('/:group/:id', instanceRoutes.findInstance);
+router.get('/:group', instanceRoutes.findGroup);
 
-// // You put the next routes here 👇
-// router.use("/auth", authRoutes);
-// router.use("/profile", isLoggedIn, profileRoutes); // added for profile page
-// router.use("/recipes", isLoggedIn, recipeRoutes); 
-// router.use("/comments", isLoggedIn, commentRoutes)
-// router.use("/upload", cloudinaryRoutes)
+// 👇 used extra /delete/ so as to make sure no errors in what is deleted from url tree
+router.delete('/delete/:group/:id', instanceRoutes.deleteInstance);
 
-// example: router.use("/auth", authRoutes)
 module.exports = router;
